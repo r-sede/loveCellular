c5246c5157670bec03efa5f5f8cf006acdfa7e2a
r-sede <r_sede@orange.fr>
2018-08-23 11:53:59 +0200

    add bomb :)

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
+local debug = true
+local showTile = true
+local showFog = true
 PPM = 4 -- scale
 BLOCKSIZE = 8
 local SWIDTH = 1024--640--*2 -- screen width in pixels
 local SHEIGHT = 768--576--*2 -- screen height in pixels
 WTWIDTH = SWIDTH/(BLOCKSIZE*PPM) -- number of blocks on screen along x
 WTHEIGHT = SHEIGHT/(BLOCKSIZE*PPM) -- number of blocks on screen along y
-WORLDWIDTH = 256*2 -- width of world in block
-WORLDHEIGHT = 256*2 -- height of world in block
+WORLDWIDTH = 256*1 -- width of world in block
+WORLDHEIGHT = 256*1 -- height of world in block
 local TILES = {}
 local ATLAS = nil
 local SEED = nil
@@ -15,10 +18,11 @@ local rRockCells = 0.48
 local nIter =11
 local startIter = 11
 local neighBorHoodThres = 5
- terrain = nil
+terrain = nil
 local fog = nil
-local debug = true
 local playerX,playerY = nil,nil
+
+local OBJTILES = {}
 
 local torchTimer = 1/4
 local torchFps = 1/4
@@ -27,9 +31,15 @@ local densTorch = 28
 local torchs = nil
 
 local chests = nil
-local densChest = 4
+local densChest = 2
+local colorGB = 4
 
-local OBJTILES = {}
+local bombAtlas = nil
+local bombTiles = {}
+local bombTimer = 1/3
+local bombFps = 1/3
+local bombFrame = 0
+local bomb = {x=-100,y=-100}
 
 
 function love.load(arg)
@@ -37,48 +47,56 @@ function love.load(arg)
   love.keyboard.setKeyRepeat(true)
   love.window.setMode(SWIDTH, SHEIGHT)
   love.window.setTitle('loveCellular')
-  love.graphics.setBackgroundColor(32/255,70/255,50/255,1)
+  --green
+  --love.graphics.setBackgroundColor(32/255,70/255,50/255,1)
+  -- gray
+  love.graphics.setBackgroundColor(61/255,61/255,61/255,1)
   
   SEED = love.timer.getTime()
   love.math.setRandomSeed(SEED)
   require'hero'
   resetMap()
+  
+  bombAtlas = love.graphics.newImage('assets/img/bomb.png')
+  bombTiles[0]=love.graphics.newQuad(0*16 , 0*16, 16, 16, bombAtlas:getDimensions())
+  bombTiles[1]=love.graphics.newQuad(1*16 , 0*16, 16, 16, bombAtlas:getDimensions())
+  bombTiles[2]=love.graphics.newQuad(2*16 , 0*16, 16, 16, bombAtlas:getDimensions())
 
   ATLAS = love.graphics.newImage('assets/img/CB-All_Temple.png')
-  TILES[0] = love.graphics.newQuad(2*8, 3*8, 8, 8, ATLAS:getDimensions())
-  TILES[-1] = love.graphics.newQuad(3*8, 3*8, 8, 8, ATLAS:getDimensions())
-  TILES[-2] = love.graphics.newQuad(2*8, 2*8, 8, 8, ATLAS:getDimensions())
+  TILES[0] = love.graphics.newQuad(2*8 +colorGB*256, 3*8, 8, 8, ATLAS:getDimensions())
+  TILES[-1] = love.graphics.newQuad(3*8+colorGB*256, 3*8, 8, 8, ATLAS:getDimensions())
+  TILES[-2] = love.graphics.newQuad(2*8+colorGB*256, 2*8, 8, 8, ATLAS:getDimensions())
   
   ----------------------------------------------
-  TILES[31] = love.graphics.newQuad(13*8, 6*8, 8, 8, ATLAS:getDimensions())
+  TILES[31] = love.graphics.newQuad(13*8+colorGB*256, 6*8, 8, 8, ATLAS:getDimensions())
   TILES[191] = TILES[31]
   TILES[63] = TILES[31]
   TILES[159] = TILES[31]
   
-  TILES[28] = love.graphics.newQuad(16*8, 8*8, 8, 8, ATLAS:getDimensions())
+  TILES[28] = love.graphics.newQuad(16*8+colorGB*256, 8*8, 8, 8, ATLAS:getDimensions())
   TILES[60] = TILES[28]
   TILES[30] = TILES[28]
   TILES[62] = TILES[28]
   TILES[188] = TILES[28]--
   TILES[156] = TILES[28]--
   
-  TILES[15] = love.graphics.newQuad(12*8, 8*8, 8, 8, ATLAS:getDimensions())
+  TILES[15] = love.graphics.newQuad(12*8+colorGB*256, 8*8, 8, 8, ATLAS:getDimensions())
   TILES[135] = TILES[15]
   TILES[7] = TILES[15]
   TILES[183] = TILES[15]--
   TILES[143] = TILES[15]
   TILES[167] = TILES[15]--
 
-  TILES[127] = love.graphics.newQuad(16*8, 6*8, 8, 8, ATLAS:getDimensions())
+  TILES[127] = love.graphics.newQuad(16*8+colorGB*256, 6*8, 8, 8, ATLAS:getDimensions())
 
-  TILES[223] = love.graphics.newQuad(12*8, 6*8, 8, 8, ATLAS:getDimensions())
+  TILES[223] = love.graphics.newQuad(12*8+colorGB*256, 6*8, 8, 8, ATLAS:getDimensions())
 
-  TILES[199] = love.graphics.newQuad(12*8, 7*8, 8, 8, ATLAS:getDimensions())
+  TILES[199] = love.graphics.newQuad(12*8+colorGB*256, 7*8, 8, 8, ATLAS:getDimensions())
   TILES[207] = TILES[199]
   TILES[239] = TILES[199]
   TILES[231] = TILES[199]
   
-  TILES[193] = love.graphics.newQuad(13*8, 10*8, 8, 8, ATLAS:getDimensions())
+  TILES[193] = love.graphics.newQuad(13*8+colorGB*256, 10*8, 8, 8, ATLAS:getDimensions())
   TILES[225] = TILES[193]
   TILES[227] = TILES[193]
   TILES[195] = TILES[193]
@@ -87,50 +105,53 @@ function love.load(arg)
   TILES[217] = TILES[193]--
   TILES[233] = TILES[193]--
 
-  TILES[247] = love.graphics.newQuad(11*8, 10*8, 8, 8, ATLAS:getDimensions())
+  TILES[247] = love.graphics.newQuad(11*8+colorGB*256, 10*8, 8, 8, ATLAS:getDimensions())
   --------------------------------------------------
   
-  TILES[241] = love.graphics.newQuad(16*8, 10*8, 8, 8, ATLAS:getDimensions())
+  TILES[241] = love.graphics.newQuad(16*8+colorGB*256, 10*8, 8, 8, ATLAS:getDimensions())
   TILES[249] = TILES[241]
   TILES[251] = TILES[241]
   TILES[243] = TILES[241]
   TILES[251] = TILES[241]
 
-  TILES[253] = love.graphics.newQuad(17*8, 10*8, 8, 8, ATLAS:getDimensions())
+  TILES[253] = love.graphics.newQuad(17*8+colorGB*256, 10*8, 8, 8, ATLAS:getDimensions())
   
-  TILES[248] = love.graphics.newQuad(15*8, 10*8, 8, 8, ATLAS:getDimensions())
+  TILES[248] = love.graphics.newQuad(15*8+colorGB*256, 10*8, 8, 8, ATLAS:getDimensions())
   TILES[240] = TILES[248]
   TILES[120] = TILES[248]
   TILES[112] = TILES[248]
   TILES[122] = TILES[248]--
   
-  TILES[124] = love.graphics.newQuad(16*8, 7*8, 8, 8, ATLAS:getDimensions())
+  TILES[124] = love.graphics.newQuad(16*8+colorGB*256, 7*8, 8, 8, ATLAS:getDimensions())
   TILES[126] = TILES[124]
   TILES[252] = TILES[124]
   TILES[254] = TILES[124]
 
   -----------------------------------
-  TILES[256] = love.graphics.newQuad(14*8,7*8,8,8,ATLAS:getDimensions())
-  TILES[257] = love.graphics.newQuad(14*8,8*8,8,8,ATLAS:getDimensions())
+  TILES[256] = love.graphics.newQuad(14*8+colorGB*256,7*8,8,8,ATLAS:getDimensions())
+  TILES[257] = love.graphics.newQuad(14*8+colorGB*256,8*8,8,8,ATLAS:getDimensions())
 
   ---------------OBJECT------------
   OBJTILES['torch'] = {}
-  OBJTILES['torch'][0] = love.graphics.newQuad(18*8,14*8,16,24,ATLAS:getDimensions())
-  OBJTILES['torch'][1] = love.graphics.newQuad(20*8,14*8,16,24,ATLAS:getDimensions())
+  OBJTILES['torch'][0] = love.graphics.newQuad(18*8+colorGB*256,14*8,16,24,ATLAS:getDimensions())
+  OBJTILES['torch'][1] = love.graphics.newQuad(20*8+colorGB*256,14*8,16,24,ATLAS:getDimensions())
 
   OBJTILES['chest'] = {}
-  OBJTILES['chest'][0] = love.graphics.newQuad(6*8, 17*8,8,8,ATLAS:getDimensions())
-  OBJTILES['chest'][1] = love.graphics.newQuad(7*8, 17*8,8,8,ATLAS:getDimensions())
-  OBJTILES['chest'][2] = love.graphics.newQuad(8*8, 17*8,8,8,ATLAS:getDimensions())
-  OBJTILES['chest'][3] = love.graphics.newQuad(9*8, 17*8,8,8,ATLAS:getDimensions())
-  OBJTILES['chest'][4] = love.graphics.newQuad(10*8, 17*8,8,8,ATLAS:getDimensions())
+  OBJTILES['chest'][0] = love.graphics.newQuad(6*8+colorGB*256, 17*8,8,8,ATLAS:getDimensions())
+  OBJTILES['chest'][1] = love.graphics.newQuad(7*8+colorGB*256, 17*8,8,8,ATLAS:getDimensions())
+  OBJTILES['chest'][2] = love.graphics.newQuad(8*8+colorGB*256, 17*8,8,8,ATLAS:getDimensions())
+  OBJTILES['chest'][3] = love.graphics.newQuad(9*8+colorGB*256, 17*8,8,8,ATLAS:getDimensions())
+  OBJTILES['chest'][4] = love.graphics.newQuad(10*8+colorGB*256, 17*8,8,8,ATLAS:getDimensions())
+
+
 end
 
 function love.update(dt)
   hero:update(dt)
   rafUtils.camera.lookAt(hero.x,hero.y)
-  updateFog(9)
+  updateFog(8)
   updateTorch(dt)
+  updateBomb(dt)
 end
 
 function love.draw()
@@ -174,6 +195,8 @@ function love.draw()
   
   drawTorch (startX, endX, startY, endY)
   drawChest (startX, endX, startY, endY)
+  --testBomb
+  love.graphics.draw(bombAtlas,bombTiles[bombFrame],bomb.x*BLOCKSIZE*PPM - rafUtils.camera.x, bomb.y*BLOCKSIZE*PPM - rafUtils.camera.y,PPM*0.5,PPM*0.5)
   --hero
   hero:draw()
  
@@ -205,7 +228,7 @@ function love.draw()
           end
         end
       end
-      if debug then 
+      if showTile then 
         love.graphics.setColor(1,0,1,1)
         
         love.graphics.print(terrain[yy][xx],xx*BLOCKSIZE*PPM - rafUtils.camera.x, yy*BLOCKSIZE*PPM - rafUtils.camera.y)
@@ -218,7 +241,7 @@ function love.draw()
       end
     end
   end
-  if true then
+  if debug then
     love.graphics.setColor(1,0,1,1)
     love.graphics.print(startIter,0,0)
     love.graphics.print(rafUtils.camera.x..' ; '..rafUtils.camera.y,0,15)
@@ -239,9 +262,19 @@ function love.keypressed (key)
   if key == 'd' then
     if debug then debug = false else debug = true end
   end
+  if key == 'f' then
+    if showFog then showFog = false else showFog = true end
+  end
+  if key == 't' then
+    if showTile then showTile = false else showTile = true end
+  end
   if key == 'return' then
     -- print(SEED)
     resetMap()
+  end
+  if key == 'b' then
+    -- print(SEED)
+    dropBomb(math.ceil(hero.x+1),math.ceil(hero.y))
   end
   if key == "s" then 
     SEED = love.timer.getTime()
@@ -271,6 +304,20 @@ function resetMap()
   createWall3(terrain)
   varyFloor(terrain)
   torchs = createTorchs(terrain,densTorch)
+  while torchs == false do
+    densTorch = densTorch - 1
+    SEED = love.timer.getTime()    
+    nIter=startIter
+    love.math.setRandomSeed(SEED)
+    terrain = createRandTerrain(WORLDWIDTH,WORLDHEIGHT)
+    terrain = iterCellular(terrain)
+    terrain = createWall(terrain)
+    terrain = createWall2(terrain)
+    createWall3(terrain)
+    varyFloor(terrain)
+    torchs = createTorchs(terrain,densTorch)
+  end
+
   chests = createChest(terrain,densChest)
 
   local xx,yy=0,0
@@ -300,18 +347,7 @@ function createTorchs(terrain, dens)
         freezeCount = freezeCount + 1
         if freezeCount >= 900000 then
           print('yes')
-          SEED = love.timer.getTime()
-          --torchs = {}
-          nIter=startIter
-          love.math.setRandomSeed(SEED)
-          terrain = createRandTerrain(WORLDWIDTH,WORLDHEIGHT)
-          terrain = iterCellular(terrain)
-          terrain = createWall(terrain)
-          terrain = createWall2(terrain)
-          createWall3(terrain)
-          varyFloor(terrain)
-          d= 0
-          return createTorchs(terrain,dens)
+          return false
         end
         local xtent = love.math.random(xxx*128,(xxx+1)*128)
         local ytent = love.math.random(yyy*128,(yyy+1)*128)
@@ -351,13 +387,17 @@ function drawTorch (startX, endX, startY, endY)
   for i=1,#torchs do
     if torchs[i].x >= startX and torchs[i].x < endX and torchs[i].y > startY-3 and torchs[i].y < endY then
       if fog[torchs[i].y][torchs[i].x] == 0 and
-        fog[torchs[i].y][torchs[i].x+1] == 0 and
-        fog[torchs[i].y+1][torchs[i].x] == 0 and
-        fog[torchs[i].y+1][torchs[i].x+1] == 0 and
-        fog[torchs[i].y+2][torchs[i].x] == 0 and
-        fog[torchs[i].y+2][torchs[i].x+1] == 0 then
+      fog[torchs[i].y][torchs[i].x+1] == 0 and
+      fog[torchs[i].y+1][torchs[i].x] == 0 and
+      fog[torchs[i].y+1][torchs[i].x+1] == 0 and
+      fog[torchs[i].y+2][torchs[i].x] == 0 and
+      fog[torchs[i].y+2][torchs[i].x+1] == 0 then
         love.graphics.draw(ATLAS,OBJTILES['torch'][torchFrame],torchs[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,torchs[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,0,PPM,PPM)
-        --love.graphics.rectangle('line',torchs[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,torchs[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,BLOCKSIZE* PPM *2,BLOCKSIZE*PPM*3)
+        if debug then 
+          love.graphics.setColor(1,0,1,1)
+          love.graphics.rectangle('line',torchs[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,torchs[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,BLOCKSIZE* PPM *2,BLOCKSIZE*PPM*3)
+          love.graphics.setColor(1,1,1,1)
+        end
       end
     end 
   end
@@ -376,7 +416,6 @@ function createChest(terrain,dens)
         if freezeCount >= 900000 then
           print('yesChest')
           SEED = love.timer.getTime()
-          --torchs = {}
           nIter=startIter
           love.math.setRandomSeed(SEED)
           terrain = createRandTerrain(WORLDWIDTH,WORLDHEIGHT)
@@ -386,8 +425,8 @@ function createChest(terrain,dens)
           createWall3(terrain)
           varyFloor(terrain)
           torchs = createTorchs(terrain,densTorch)
-          d=0
-          return createChest(terrain,dens)
+          createChest(terrain,dens)
+          return
         end
         local xtent = love.math.random(xxx*128,(xxx+1)*128)
         local ytent = love.math.random(yyy*128,(yyy+1)*128)
@@ -405,7 +444,7 @@ function createChest(terrain,dens)
           print(ytent)
           print() ]]
           if notFound then
-            table.insert( res, {x=xtent,y=ytent,open=false,inside={'lol'},frame=4} )
+            table.insert( res, {x=xtent,y=ytent,open=false,inside={'lol'},frame=0} )
             --print(xtent)
             table.insert(chestPlacedX,{x=xtent,y=ytent})
             terrain[ytent][xtent] = 1
@@ -423,9 +462,21 @@ function drawChest (startX, endX, startY, endY)
     if chests[i].x >= startX and chests[i].x < endX and chests[i].y > startY-3 and chests[i].y < endY then
       if fog[chests[i].y][chests[i].x] == 0 then
         love.graphics.draw(ATLAS,OBJTILES['chest'][chests[i].frame],chests[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,chests[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,0,PPM,PPM)
-        --love.graphics.rectangle('line',torchs[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,torchs[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,BLOCKSIZE* PPM *2,BLOCKSIZE*PPM*3)
+        if debug then
+          love.graphics.setColor(1,0,1,1)
+          love.graphics.rectangle('line',chests[i].x * PPM * BLOCKSIZE - rafUtils.camera.x,chests[i].y * PPM * BLOCKSIZE - rafUtils.camera.y,BLOCKSIZE* PPM,BLOCKSIZE*PPM)
+          love.graphics.setColor(1,1,1,1)
+        end
       end
     end 
+  end
+end
+
+function updateBomb(dt)
+  bombTimer = bombTimer - dt
+  if bombTimer <= 0 then
+    bombFrame = (bombFrame + 1)%(#bombTiles+1)
+    bombTimer = bombFps
   end
 end
 
@@ -435,7 +486,8 @@ function createFog (width,height)
   for yy=0,height-1 do
     fog[yy] = {}
     for xx=0,width-1 do
-    fog[yy][xx] = 1
+
+    fog[yy][xx] = showFog and 1 or 0
     end
   end
   return fog
@@ -607,9 +659,147 @@ function createWall3(terrain)
         elseif yy <= #terrain -1 then 
           terrain[yy+1][xx] = 256
         end
-      else
       end
     end
   end
   return terrain
+end
+
+function dropBomb(x,y)
+  bomb.x,bomb.y = x,y
+  local dist = 2
+  local startX = (x - dist-2)
+  local endX = (x + dist)
+  local startY = (y - dist-2)
+  local endY = (y + dist)
+
+  startY = rafUtils.clamp(startY,0,#terrain)
+  startX = rafUtils.clamp(startX,0,#terrain[startY])
+  endY = rafUtils.clamp(endY,0,#terrain)
+  endX = rafUtils.clamp(endX,0,#terrain[endY])
+
+  for yy = startY,endY do
+    for xx = startX,endX do
+      terrain [yy][xx] = 0
+    end
+  end
+
+  startY = rafUtils.clamp(startY-3,0,#terrain)
+  startX = rafUtils.clamp(startX-3,0,#terrain[startY])
+  endY = rafUtils.clamp(endY+3,0,#terrain)
+  endX = rafUtils.clamp(endX+3,0,#terrain[endY])
+
+  local tmp = {}
+  for yy=startY,endY do
+    tmp[yy] = {}
+    for xx=startX,endX do
+      if terrain[yy][xx] == 0 then
+        local l = terrain[yy]                   [(xx-1)%(#terrain[yy]+1)]
+        local lu = terrain[(yy-1)%(#terrain+1)] [(xx-1)%(#terrain[yy]+1)]
+        local u = terrain[(yy-1)%(#terrain+1)]  [xx]
+        local ru = terrain[(yy-1)%(#terrain+1)] [(xx+1)%(#terrain[yy]+1)]
+        local r = terrain[yy]                   [(xx+1)%(#terrain[yy]+1)]
+        local rd = terrain[(yy+1)%(#terrain+1)] [(xx+1)%(#terrain[yy]+1)]
+        local d = terrain[(yy+1)%(#terrain+1)]  [xx]
+        local ld = terrain[(yy+1)%(#terrain+1)] [(xx-1)%(#terrain[yy]+1)]
+        -- local neighBorSum = l+lu+u+ru+r+rd+d
+        local neighBorSum = 0
+        if l == 1 then neighBorSum = neighBorSum + 1 end
+        if lu == 1 then neighBorSum = neighBorSum + 1 end
+        if u == 1 then neighBorSum = neighBorSum + 1 end
+        if ru == 1 then neighBorSum = neighBorSum + 1 end
+        if r == 1 then neighBorSum = neighBorSum + 1 end
+        if rd == 1 then neighBorSum = neighBorSum + 1 end
+        if d == 1 then neighBorSum = neighBorSum + 1 end
+        if ld == 1 then neighBorSum = neighBorSum + 1 end
+        -- tmp[yy][xx] = neighBorSum >= neighBorHoodThres and 1 or 0
+        if neighBorSum >= 1 then tmp[yy][xx]=2 else tmp[yy][xx] = terrain[yy][xx] end
+      else tmp[yy][xx] = terrain[yy][xx]
+      end
+    end
+  end
+
+  for yy=startY,endY do
+    for xx=startX,endX do
+      terrain[yy][xx]=tmp[yy][xx]
+    end
+  end
+
+  tmp = {}
+  for yy=startY,endY do
+    tmp[yy] = {}
+    for xx=startX,endX do
+      if terrain[yy][xx] == 2 then
+        local l = terrain[yy]                   [(xx-1)%(#terrain[yy]+1)]
+        local lu = terrain[(yy-1)%(#terrain+1)] [(xx-1)%(#terrain[yy]+1)]
+        local u = terrain[(yy-1)%(#terrain+1)]  [xx]
+        local ru = terrain[(yy-1)%(#terrain+1)] [(xx+1)%(#terrain[yy]+1)]
+        local r = terrain[yy]                   [(xx+1)%(#terrain[yy]+1)]
+        local rd = terrain[(yy+1)%(#terrain+1)] [(xx+1)%(#terrain[yy]+1)]
+        local d = terrain[(yy+1)%(#terrain+1)]  [xx]
+        local ld = terrain[(yy+1)%(#terrain+1)]  [(xx-1)%(#terrain[yy]+1)]
+        -- local neighBorSum = l+lu+u+ru+r+rd+d
+        local neighBorSum = 0
+        if l >= 1 or l == 2 then neighBorSum = neighBorSum + 1 end
+        if lu >= 1 or lu == 2 then neighBorSum = neighBorSum + 2 end
+        if u >= 1 or u == 2 then neighBorSum = neighBorSum + 4 end
+        if ru >= 1 or ru == 2 then neighBorSum = neighBorSum + 8 end
+        if r >= 1 or r == 2 then neighBorSum = neighBorSum + 16 end
+        if rd >= 1 or rd == 2 then neighBorSum = neighBorSum + 32 end
+        if d >= 1 or d == 2 then neighBorSum = neighBorSum + 64 end
+        if ld >= 1 or ld == 2 then neighBorSum = neighBorSum + 128 end
+
+        --print(neighBorSum)
+        -- tmp[yy][xx] = neighBorSum >= neighBorHoodThres and 1 or 0
+        if neighBorSum >= 1 then tmp[yy][xx]=neighBorSum else tmp[yy][xx] = terrain[yy][xx] end
+      else tmp[yy][xx] = terrain[yy][xx]
+      end
+    end
+  end
+
+  for yy=startY,endY do
+    for xx=startX,endX do
+      terrain[yy][xx]=tmp[yy][xx]
+    end
+  end
+
+  for yy=startY,endY do
+    for xx=startX,endX do
+      if terrain[yy][xx] == 0 then
+        local rand = love.math.random()
+        if rand < 0.005 then  
+          terrain[yy][xx]=-1
+        elseif rand < 0.01 then
+          terrain[yy][xx]=-2
+        else
+          terrain[yy][xx]=0
+        end
+      end
+    end
+  end
+
+  for yy=startY,endY do
+    for xx=startX,endX do
+      if  terrain[yy][xx] == 31 or
+          terrain[yy][xx] == 191 or
+          terrain[yy][xx] == 63 or
+          terrain[yy][xx] == 159 or
+          terrain[yy][xx] == 135 or
+          terrain[yy][xx] == 15 or
+          terrain[yy][xx] == 7 or
+          terrain[yy][xx] == 143 or
+          terrain[yy][xx] == 28 or
+          terrain[yy][xx] == 60 or
+          terrain[yy][xx] == 30 or
+          terrain[yy][xx] == 62 then
+        if yy <= #terrain -2 then 
+          terrain[yy+1][xx] = 256
+          terrain[yy+2][xx] = 257
+        elseif yy <= #terrain -1 then 
+          terrain[yy+1][xx] = 256
+        end
+      end
+    end
+  end
+
 end
